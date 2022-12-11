39037f3be948679e3567cceecf0a0d8b5bee561c
sncfsamy <samy.lamiri@sncf.fr>
2022-12-11 13:23:51 +0100

    Added color change on online players.

--- src/components/scores/scores.jsx
+++ src/components/scores/scores.jsx
@@ -2,11 +2,17 @@ import React, {useContext} from "react";
 import './scores.css';
 import Context from '../../context';
 import Score from './score';
+import useColorChange from 'use-color-change';
 
 const Scores = ({inSimulation, playersOnline}) => {
     const { fighters } = useContext(Context);
+    const onlinePlayers = useColorChange(playersOnline, {
+        higher: '#26e026',
+        lower: '#ee1f1f',
+        duration: 10000
+    });
     return  <div className="scores">
-                <span>{playersOnline + (playersOnline > 1 ? " joueurs en ligne" : " joueur en ligne")}</span>
+                <span><b style={onlinePlayers}>{playersOnline}</b> {playersOnline > 1 ? " joueurs en ligne" : " joueur en ligne"}</span>
                 <h2>Scores:</h2>
                 <div className="scoreslist">
                     <div><span><u>Combattants</u></span></div><div><span className="hero">🏆</span></div><div><span className="hero">💀</span></div>
